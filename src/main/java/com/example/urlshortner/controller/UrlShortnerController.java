1e732e360e7f643cc511f1ac93237ee6bc6c9699
Shreypatel911 <77115893+Shreypatel911@users.noreply.github.com>
2023-11-22 00:05:07 -0700

    complete backend

--- src/main/java/com/example/urlshortner/controller/UrlShortnerController.java
+++ src/main/java/com/example/urlshortner/controller/UrlShortnerController.java
@@ -5,12 +5,19 @@ import com.example.urlshortner.model.UrlDto;
 import com.example.urlshortner.model.UrlErrorResponseDto;
 import com.example.urlshortner.model.UrlResponseDto;
 import com.example.urlshortner.service.UrlService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Controller
 public class UrlShortnerController {
@@ -35,5 +42,37 @@ public class UrlShortnerController {
         urlErrorResponseDto.setStatus("400");
 
         return new ResponseEntity<>(urlErrorResponseDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{shortLink}")
+    public ResponseEntity<?> openOriginalLink(@PathVariable String shortLink, HttpServletResponse response) throws IOException {
+        if(StringUtils.isEmpty(shortLink)){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setStatus("400");
+            urlErrorResponseDto.setError("Invalid Url");
+
+            return new ResponseEntity<>(urlErrorResponseDto, HttpStatus.OK);
+        }
+
+        Url url = urlService.getEncodedUrl(shortLink);
+        if(url == null){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setStatus("400");
+            urlErrorResponseDto.setError("This url is not present in the db");
+
+            return new ResponseEntity<>(urlErrorResponseDto, HttpStatus.OK);
+        }
+
+        if(url.getExpirationDate().isBefore(LocalDateTime.now())){
+            urlService.deleteShortLink(url);
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setStatus("400");
+            urlErrorResponseDto.setError("This url is expired");
+
+            return new ResponseEntity<>(urlErrorResponseDto, HttpStatus.OK);
+        }
+
+        response.sendRedirect(url.getOriginalLink());
+        return null;
     }
 }
